4c8379d5054131a29d1e4af2fc3b30fcae360359
Peanut Paste <isaacjingyi1997@gmail.com>
2023-09-11 23:05:12 +0800

    Added bonus, ft_lstadd_front and ft_lstnew

--- done/ft_lstadd_front_bonus.c
+++ done/ft_lstadd_front_bonus.c
@@ -1,19 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jingtan <jingtan@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/11 19:02:12 by jingtan           #+#    #+#             */
-/*   Updated: 2023/09/11 19:02:12 by jingtan          ###   ########.fr       */
+/*   Created: 2023/09/11 22:59:13 by jingtan           #+#    #+#             */
+/*   Updated: 2023/09/11 22:59:13 by jingtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putendl_fd(char *s, int fd)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	write(fd, &s, ft_strlen(s));
-	write(fd, '\n', 1);
+	if (lst && new)
+	{
+		if (*lst)
+		{
+			new->next = *lst;
+			*lst = new;
+		}
+	}
 }
--- done/ft_lstnew_bonus.c
+++ done/ft_lstnew_bonus.c
@@ -1,19 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jingtan <jingtan@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/11 19:02:12 by jingtan           #+#    #+#             */
-/*   Updated: 2023/09/11 19:02:12 by jingtan          ###   ########.fr       */
+/*   Created: 2023/09/11 22:50:10 by jingtan           #+#    #+#             */
+/*   Updated: 2023/09/11 22:50:10 by jingtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putendl_fd(char *s, int fd)
+t_list	*ft_lstnew(void *content)
 {
-	write(fd, &s, ft_strlen(s));
-	write(fd, '\n', 1);
+	t_list	*res;
+
+	res = malloc(sizeof(t_list));
+	if (!res)
+		return (NULL);
+	res->content = content;
+	res->next = NULL;
+	return (res);
 }
