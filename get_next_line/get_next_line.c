a99a19254f709624087b061185f6f92ba612dced
Jing Yi Tan <jingtan@c2r4s11.42singapore.sg>
2023-09-15 01:57:32 +0800

    To resume at home, sleep deprived, barely started

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,4 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*get_next_line(int fd)
+{
 
+}
+
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int	fd;
+	char	*a;
+
+	fd = open("test.txt", O_RDONLY);
+	a = get_next_line(fd);
+	printf("%s", a);
+	return (0);
+}
