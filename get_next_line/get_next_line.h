796ef078ecc761dfeb277a52593e6036a546ecf9
Jing Yi Tan <jingtan@c2r4s11.42singapore.sg>
2023-09-22 15:15:18 +0800

    Updated header

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jingtan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 23:57:56 by jingtan           #+#    #+#             */
-/*   Updated: 2023/09/14 23:57:57 by jingtan          ###   ########.fr       */
+/*   Updated: 2023/09/22 15:10:15 by jingtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 size_t	ft_strlen(char *s, int index);
 size_t	ft_getnlnt(char *s);
